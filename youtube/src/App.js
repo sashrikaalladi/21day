7f9a9c091af7bd4f392abc2f1634763429ccd9f0
sashrikaalladi <sashrikaalladi@gmail.com>
2023-01-05 18:54:54 +0530

    up

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,31 +1,34 @@
-
+import React, { useContext } from 'react';
 import './App.css';
-import React from 'react';
 import Header from './pages/Header/Header'
 import Sidebar from './pages/sidebar/Sidebar';
 import Videos from './pages/videos/Videos';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Subscriptions from './pages/subscriptions/Subscriptions';
+import { Themer } from './pages/usetheme/Themer';
 
 
 function App() {
   return (
-    <div className="App">
-      
+  
+      <div className="App">
+          <Themer>
       <Header />
-      <div className='app__page'>
-        <BrowserRouter>
-          <Sidebar />
-          <Routes>
-          <Route path="/" element={<Videos />} />
-            <Route path="/Subscriptions" element={<Subscriptions />} />
-            <Route path="/Videos" element={<Videos />} />
+          <div className='app__page'>
+            <BrowserRouter>
+              <Sidebar />
+              <Routes>
+                <Route path="/" element={<Videos />} />
+                <Route path="/Subscriptions" element={<Subscriptions />} />
+                <Route path="/Videos" element={<Videos />} />
+              </Routes>
+
+            </BrowserRouter>
            
-          </Routes>
-          
-        </BrowserRouter>
+          </div>
+          </Themer>
         </div>
-    </div>
+
 
   );
 }
